61b50ac280bd2b9544fe943a1105905f2459c7c1
Amin Abbaspour <a.abbaspour@gmail.com>
2021-03-17 12:22:56 +1100

    baseline bundles listing working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import * as React from "react";
 import {Admin, Resource, ListGuesser} from 'react-admin';
-import jsonServerProvider from 'ra-data-json-server';
-import {withAuth0, withAuthenticationRequired} from "@auth0/auth0-react";
 
-const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+import {useAuth0, withAuth0, withAuthenticationRequired} from "@auth0/auth0-react";
+import repoProvider from "./providers/repositoryProvider";
 
-const App = () => (
-    <Admin dataProvider={dataProvider}>
-        <Resource name="users" list={ListGuesser}/>
-    </Admin>
-);
+const App = () => {
+    const { getAccessTokenSilently } = useAuth0();
+    const dataProvider = repoProvider(getAccessTokenSilently);
+
+    return (
+        <Admin dataProvider={dataProvider}>
+            <Resource name="bundles" list={ListGuesser}/>
+        </Admin>
+    );
+}
 
 export default withAuthenticationRequired(withAuth0(App));
---
+++ tunnel/readme.md
@@ -0,0 +1,1 @@
+openssl req -new -x509 -days 365 -nodes -out st.pem -keyout st.pem
